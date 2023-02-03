17dd8323df4201059931491dba66b4919d687f84
S.Mehrotra <sanjmeh@gmail.com>
2023-02-04 02:33:31 +0530

    updated with 6 profiles, from 4 and split option 3 & 4 into diff recos.

--- main.R
+++ main.R
@@ -3,17 +3,21 @@
 # 
 pacman::p_load(shiny,shinydashboard,readxl, shinydashboardPlus,googlesheets4,tidyverse,janitor,data.table)
 # read the file that contains question class serial number borders
-qclass <- fread("question_class.txt")
+qclass <- fread("question_class.txt",header = T)
 glink <- readLines("links.txt")[1]
+# read the sentences that have to be affixed in recommentation - one time read.
+sndt <- read_excel("SoME Communication Profile Descriptions.xlsx",sheet = "Final") %>% as.data.table()
+strt <- "2023-02-03"
 
-# read and melt survey results
-x1 <- read_excel("Survey Responses-SoMECommunicationProfileVersion10.xlsx",sheet = 1,skip = 0) %>% 
+# read and melt survey results - both sheets 1 & 2 separately and then join
+x1 <- read_excel("Survey Responses-SoMECommunicationProfileVersion20.xlsx",sheet = 1,skip = 0) %>% 
         as.data.table() %>% clean_names()
-x2 <- read_excel("Survey Responses-SoMECommunicationProfileVersion10.xlsx",sheet = 2,skip = 1)%>% 
+x2 <- read_excel("Survey Responses-SoMECommunicationProfileVersion20.xlsx",sheet = 2,skip = 1)%>% 
         as.data.table() %>% 
         row_to_names(1) %>% 
         clean_names() %>% 
         select(1,matches("x\\d+_")) %>% tail(-1) %>% 
+        select(-starts_with(c("x31","x32"))) %>% # ignoring the two columns after 30, If there are more just add x33, x34 etc.
         rename(name = question) %>% 
         left_join(x1,by = "name")
 scoredt <- melt.data.table(x2,id.vars = c("name","email","submitted_on"))
@@ -23,41 +27,53 @@ scoredt[,qtype := case_when(
         seq <= qclass[[1]][2] ~ qclass[[2]][2],
         seq <= qclass[[1]][3] ~ qclass[[2]][3],
         seq <= qclass[[1]][4] ~ qclass[[2]][4],
+        seq <= qclass[[1]][5] ~ qclass[[2]][5],
+        seq <= qclass[[1]][6] ~ qclass[[2]][6],
         T ~ "no class"
         ),name]
 #scoredt[,qcompr:=variable  %>% str_remove("^x\\d+_") %>% str_replace_all("_"," ") %>% str_remove("[:punct:]")]
 
 # read database of sentennces
-sntdt <- read_sheet(glink,col_names = F) %>% as.data.table() 
-sndt2 <- sntdt[-1] %>% clean_names()  %>% filter(!is.na(x2)) %>% select(x1:x4)
+sndt2 <- sndt %>% clean_names()  %>%  select(1:5) %>% filter(!is.na(advanced_scores_5))
+setDT(sndt2)
 sndt2[, seq := seq_len(.N)]
 sndt2[,qtype := case_when(
         seq <= qclass[[1]][1] ~ qclass[[2]][1],
         seq <= qclass[[1]][2] ~ qclass[[2]][2],
         seq <= qclass[[1]][3] ~ qclass[[2]][3],
         seq <= qclass[[1]][4] ~ qclass[[2]][4],
+        seq <= qclass[[1]][5] ~ qclass[[2]][5],
+        seq <= qclass[[1]][6] ~ qclass[[2]][6],
         T ~ "no class")
 ]
 
 master <- sndt2[scoredt,on = "seq",nomatch = NA]
-if(master[is.na(x1),.N] > 0) message("Warning: Atleast one question string did not match")
+#if(master[is.na(x1),.N] > 0) message("Warning: Atleast one question string did not match")
 master[,reco:=case_when(
-        value==5 ~ x4,
-        value %in% c(3,4) ~ x3,
-        value %in% c(1,2) ~ x2,
+        value==5 ~ advanced_scores_5,
+        value == 4 ~ intermediate_scores_4,
+        value == 3 ~ intermediate_scores_3,
+        value %in% c(1,2) ~ low_scores_1_2,
         T ~ "NOT FOUND"
 )]
-master[,reco:=str_remove(reco,"\\.$")]
+master[,reco:=str_squish(reco) %>% str_remove("\\.$")]
 
-final <- master[,para:=paste(reco,collapse = ". "),.(name,qtype,email,submitted_on)] %>% 
+final <- master[,.(name,email,submitted_on,qtype,reco)] %>% 
         unique() %>% 
-        mutate(para = paste0(para,".")) %>% 
-        select(name,email,submitted_on,qtype,para)
+        group_by(qtype,name) %>% 
+        mutate(para=paste(reco,collapse = ".\n*  "), para = paste0("*  ",para,".")) %>% 
+        ungroup() %>% 
+        select(name,email,submitted_on,qtype,para) %>% unique() %>% 
+        as.data.table()
         
-final %>% 
+final2 <- 
+        final %>% 
         dcast.data.table(name + email + submitted_on ~ qtype,value.var = "para") %>% 
-        rename(Learner = name) %>%
-        write_sheet(ss = glink,sheet = "recos")
+        rename(Learner = name) %>% 
+        mutate(date = lubridate::dmy(submitted_on)) %>% 
+        filter(date >= strt)
+
+write_sheet(final2,ss = glink,sheet = "test2")
 
 message(paste("Loaded a total of ",nrow(final)," results across ",final[,uniqueN(name)]," students"))
 print(final[,unique(name)])
